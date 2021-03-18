1047ced90f84c64bbcbde51943b3596babb9b341
Sergey <s.koldaev@haulmont.com>
2021-03-18 10:03:36 +0300

    Remove unused import

--- modules/jm/src/com/haulmont/addon/cubajm/JavaMelodyConfig.java
+++ modules/jm/src/com/haulmont/addon/cubajm/JavaMelodyConfig.java
@@ -21,7 +21,6 @@ import com.haulmont.cuba.core.config.Config;
 import com.haulmont.cuba.core.config.Property;
 import com.haulmont.cuba.core.config.Source;
 import com.haulmont.cuba.core.config.SourceType;
-import com.haulmont.cuba.core.config.defaults.DefaultBoolean;
 import com.haulmont.cuba.core.config.defaults.DefaultString;
 
 /**
